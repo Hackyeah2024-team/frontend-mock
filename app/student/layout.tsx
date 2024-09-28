aa9fcbf41d8f9a2f698bad96413da3f5d88fa1c2
Oliwier Michalik <oliwiermichalik@Oliwiers-MacBook-Pro.local>
2024-09-29 00:47:47 +0200

    fix: scroll now does not overflow on layout page

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -49,7 +49,7 @@ export default function StudentAppShell({
   // const { isDarkMode, toggle: toggleDarkMode } = useDarkMode();
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-screen max-h-screen overflow-hidden w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full flex-col gap-2">
           <div className="flex h-[40px] items-center border-b px-4 lg:h-[60px] lg:px-6">
