f9aa0ea951ff842e4207714249d86c8e912634fd
jj15warrior <janek302010@gmail.com>
2024-09-29 04:34:14 +0200

    json z /teacher/students

--- components/teacher/StudentsTable.tsx
+++ components/teacher/StudentsTable.tsx
@@ -10,11 +10,11 @@ import {
 import GradeDetails from "../student/GradeDetails";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
+import Grade from "../student/GradeDetails"
 
-export default function StudentsTable() {
+export default function StudentsTable({data} : {data: {grade: Grade, teacher: string, subject: string, student: string, problem: string}[]}) {
   return (
     <Table>
-      <TableCaption>Uczniowie</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="max-w-12">Imię i nazwisko</TableHead>
@@ -24,32 +24,25 @@ export default function StudentsTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell className="font-medium max-w-12">Adam Kowalski</TableCell>
-          <TableCell className="font-medium text-center flex gap-1 justify-start flex-wrap">
-            <GradeDetails
-              grade={{
-                grade: 2,
-                desc: "Ocena śródroczna",
-                semester: "IR",
-                type: {
-                  desc: "ocena śródroczna",
-                  weight: 3,
-                  date: "2024-12-22T15:00:00",
-                },
-              }}
-              teacher="Andrzej Górny"
-              subject="Historia"
-            />
-          </TableCell>
-          <TableCell className="max-w-12">
-            <div className="flex flex-row items-center gap-x-2">
-              <Badge className="bg-muted">Rewolucja...</Badge>
-              <Badge>+2</Badge>
-            </div>
-          </TableCell>
-          <TableCell className="text-center max-w-8">-</TableCell>
-        </TableRow>
+      {data.map(({grade, teacher, subject, student, problem},i) => (
+        <TableRow key={i}>
+            <TableCell className="font-medium max-w-12">{student}</TableCell>
+            <TableCell className="font-medium text-center flex gap-1 justify-start flex-wrap">
+              <GradeDetails
+                grade={grade}
+                teacher={teacher}
+                subject={subject}
+              />
+            </TableCell>
+            <TableCell className="max-w-12">
+              <div className="flex flex-row items-center gap-x-2">
+                <Badge className="bg-muted">{problem}</Badge>
+                {grade.grade <= 4 ? <Badge className="bg-primary">+1</Badge>:""}
+              </div>
+            </TableCell>
+            <TableCell className="text-center max-w-8">-</TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
